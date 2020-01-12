f07ad362040d8c4ece4b4b63617b8f77ba221112
Jin Yi <jinyikb@users.noreply.github.com>
2020-01-11 21:47:58 -0500

    database

--- server/server.js
+++ server/server.js
@@ -1,21 +1,22 @@
 const express = require('express')
+const app = express()
 const MongoClient = require('mongodb').MongoClient;
 
-const uri = "mongodb+srv://user:<password>@cluster0-ivfom.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://usr:pass@cluster0-ivfom.gcp.mongodb.net/todo";
 const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
+let db;
 
-  const collection = client.db("todo")
+client.connect((err) => {
+  console.log(err)
+  db = client.db("todo")
 
   const PORT = process.env.port || 3000 // Temp port
   app.listen(PORT, () => console.log(`Todo app listening on port ${PORT}`))
   
-  // perform actions on the collection object
-  client.close();
 });
 
 
-const app = express()
+
 
 /* Routes */
 app.use('/', (req, res) => {
