231bd010e389564ac7b2d4baecc6cea96eebee0e
Jin Yi <jinyikb@users.noreply.github.com>
2020-01-12 01:44:05 -0500

    server.js fix

--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+const PORT = process.env.port || 3000 // Temp port
+app.use(cors())
+app.use(express.json())
+app.listen(PORT, () => console.log(`Todo app listening on port ${PORT}`))
+
 const MongoClient = require('mongodb').MongoClient;
 
 const uri = "mongodb+srv://usr:pass@cluster0-ivfom.gcp.mongodb.net/todo";
@@ -7,35 +14,44 @@ const client = new MongoClient(uri, { useNewUrlParser: true });
 let db;
 
 client.connect((err) => {
-  console.log(err)
-  db = client.db("todo")
-
-  const PORT = process.env.port || 3000 // Temp port
-  app.listen(PORT, () => console.log(`Todo app listening on port ${PORT}`))
-  
+    console.log(err)
+    files = client.db("todo").collection('files')
 });
 
-
-
-
-/* Routes */
+/* Routes 
 app.use('/', (req, res) => {
     if (req.method == 'GET') {
         console.log(req.url) 
         res.send(`${req.url}`)
     }
 })
+*/
+
+app.get('/:id', (req, res) => {
+    console.log(req.params.id)
+    files.findOne(
+        { id: parseInt(req.params.id) }
+    ).then((document, err) => {
+        if (err) { console.log("[-] " + err) }
+        console.log("[+] " + document);
+        res.json({
+            data: document['data']
+        })
+    }).catch((err) => {
+        console.log("[-] " + err)
+    })
+
+})
 
 app.post('/upload', (req, res) => {
+    console.log(req.body)
     let file = {
-      id: Math.floor((Math.random()*1000)),
-      data: req.body.data
+        id: Math.floor((Math.random() * 1000)),
+        data: req.body.data
     }
-    db.collection('files').insertOne(file, (err, result) => {
-      if (err) return console.log(err)
+    files.insertOne(file, (err, result) => {
+        if (err) return console.log(err)
     })
-    console.log(req)
-    res.send(`Request received`)
     res.redirect('/')
 })
 
