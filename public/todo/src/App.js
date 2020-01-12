08a8b59e75cf44cf3e2593fbd847df1773f85a7f
Jin Yi <jinyikb@users.noreply.github.com>
2020-01-12 00:37:16 -0500

    textarea not updating fix

--- public/todo/src/App.js
+++ public/todo/src/App.js
@@ -7,15 +7,45 @@ const SERVER_URL = config.server_url;
 class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+    this.state = {
+      value: ""
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
     this.submit = this.submit.bind(this);
+    this.loadFile = this.loadFile.bind(this);
+
+    this.loadFile();
   }
+  
+  loadFile() {
+    let fileurl = window.location.href.split('//localhost:3001/')
+
+    if (fileurl.length > 1) {
+      
+      fetch(SERVER_URL + fileurl[1], {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          }
+        }).then((response) => response.json())
+        .then((data) => {
+          this.setState({
+            value: data.data
+          })
+        })
+        
+    }
+
+  }
+  
 
   handleChange(event) {
-    this.setState({ value: event.target.value }); //TODO: handle submit asynchronously
+    this.setState({
+      value: event.target.value
+    }); //TODO: handle submit asynchronously
   }
 
   handleKeyDown(event) {
@@ -38,13 +68,15 @@ class Form extends React.Component {
     console.log(SERVER_URL)
 
     fetch(SERVER_URL + 'upload', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ data: this.state.value })
-    })
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          data: this.state.value
+        })
+      })
       .then((response) => response.json())
       .then((responseJson) => {
         this.updateURL(responseJson.body.url);
@@ -55,11 +87,9 @@ class Form extends React.Component {
 
   render() {
 
-    return (
-      <div class="container">
-        <textarea onKeyDown={this.handleKeyDown} onChange={this.handleChange} >
-          {this.state.value}
-        </textarea>
+    return ( 
+      <div class = "container" >
+        <textarea value= {this.state.value} onKeyDown = {this.handleKeyDown} onChange = {this.handleChange} /> 
       </div>
     )
   }
