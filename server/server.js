9232b3ea520726180238f9eb6c2638fbb0323a01
Jin Yi <39113059+jinyic@users.noreply.github.com>
2020-01-15 23:43:57 -0500

    prettier

--- server/server.js
+++ server/server.js
@@ -1,79 +1,85 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
 }
-const express = require('express')
-const cors = require('cors')
-const hash = require('./hash.js')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const hash = require("./hash.js");
+const app = express();
 
-const PORT = process.env.PORT || 80
-app.use(cors())
-app.use(express.json())
-app.listen(PORT, () => console.log(`Todo app listening on port ${PORT}`))
+const PORT = process.env.PORT || 80;
+app.use(cors());
+app.use(express.json());
+app.listen(PORT, () => console.log(`Todo app listening on port ${PORT}`));
 
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
 const uri = process.env.DATABASE_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true });
 let db;
 
-client.connect((err) => {
-    console.log(err)
-    files = client.db("todo").collection('files')
+client.connect(err => {
+    console.log(err);
+    files = client.db("todo").collection("files");
 });
 
-app.get('/', (req, res) => {
-    res.status(200)
-})
+app.get("/", (req, res) => {
+    res.status(200);
+});
 
-app.get('/raw/:id', (req, res) => {
-    console.log(req.params.id)
-    files.find(
-        { _id: req.params.id }
-    ).limit(1).forEach((document, err) => {
-        if (err) { console.log(err) };
-        // TODO make sure document isnt empty
-        if (document) {
-            res.send(document.data)
-        } else {
-            res.sendStatus(301)
-        }
-    }).catch((err) => {
-        console.log(err)
-    })
-})
+app.get("/raw/:id", (req, res) => {
+    console.log(req.params.id);
+    files
+        .find({ _id: req.params.id })
+        .limit(1)
+        .forEach((document, err) => {
+            if (err) {
+                console.log(err);
+            }
+            // TODO make sure document isnt empty
+            if (document) {
+                res.send(document.data);
+            } else {
+                res.sendStatus(301);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
 
-app.get('/:id', (req, res) => {
-    files.find(
-        { _id: req.params.id }
-    ).limit(1).forEach((document, err) => {
-        if (err) { console.log(err) };
-        // console.log(document)
-        // TODO make sure document isnt empty
-        if (document) {
-            res.json({
-                data: document.data
-            })
-        } else {
-            res.sendStatus(301)
-        }
-    }).catch((err) => {
-        console.log(err)
-    })
+app.get("/:id", (req, res) => {
+    files
+        .find({ _id: req.params.id })
+        .limit(1)
+        .forEach((document, err) => {
+            if (err) {
+                console.log(err);
+            }
+            // console.log(document)
+            // TODO make sure document isnt empty
+            if (document) {
+                res.json({
+                    data: document.data
+                });
+            } else {
+                res.sendStatus(301);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
 
-})
-
-app.post('/upload', (req, res) => {
-    let id = hash.hash58()
+app.post("/upload", (req, res) => {
+    let id = hash.hash58();
     let file = {
         _id: id,
         data: req.body.data
-    }
+    };
     files.insertOne(file, (err, result) => {
         res.send({
             id: id
-        })
-        if (err) return console.log(err)
-    })
-})
-
+        });
+        if (err) return console.log(err);
+    });
+});
