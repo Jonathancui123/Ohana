146626bf8df70c12910553b602bfce001369fa34
Tony Zhao <41414030+tawnyzhao@users.noreply.github.com>
2020-03-05 01:15:25 -0500

    Add editor.js for new text area

---
+++ client/src/components/Editor.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import AceEditor from 'react-ace';
+
+import "ace-builds/src-noconflict/theme-tomorrow_night";
+
+export default class Editor extends Component {
+    render() {
+        return (
+            <div>
+                <AceEditor
+                    focus //Autofocus
+                    placeholder={this.props.placeholder}
+                    value={this.props.value}
+                    onChange={this.props.onChange}
+                    mode='text'
+                    theme='tomorrow_night'
+                    commands={[
+                        {
+                            name: 'save',
+                            bindKey: {win: 'Ctrl-s', mac: 'Command-s'},
+                            exec: this.props.submit
+                        }
+                    ]}
+                    name="paste-pal-editor"
+                    showGutter={true}
+                    editorProps={{ $blockScrolling: true }}
+                    wrapEnabled={true}
+                    showPrintMargin={false}
+                    height={"550px"}
+                    width={"100%"} />
+            </div>
+        );
+    }
+}
