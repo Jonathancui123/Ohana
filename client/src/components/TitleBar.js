d4307670ab19e3889fb09b60cf65b52b0ef526b5
Jin Yi Li <39113059+jinyic@users.noreply.github.com>
2020-03-05 02:11:19 -0500

    title css

--- client/src/components/TitleBar.js
+++ client/src/components/TitleBar.js
@@ -6,8 +6,10 @@ export default class TitleBar extends React.Component {
     render() {
         return (
             <div className="title-bar">
-                <img className="logo" src={Logo} alt="" onClick={this.props.handleClick} />
-                <h1>paste</h1><span className="spacing" /><h1>pal</h1>
+                <div id = "title" onClick={this.props.handleClick}>
+                    <img className = "logo" src = {Logo} alt = ""/>
+                    <h1>paste pal</h1>
+                </div>
             </div>
         );
     }
