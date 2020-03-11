f7f1ceeb1eeee45ad5ec5fe3e6a5bc28e349a3dd
Tony Zhao <41414030+tawnyzhao@users.noreply.github.com>
2020-03-11 10:56:38 -0400

    Add mode select

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -13,6 +13,12 @@ export default class Menu extends React.Component {
           <div className='menu'>
             <Icon id='copy' onClick={this.props.copyClipboard} className="fas fa-copy"/>
             <Icon id='save' onClick={this.props.submit} className="fas fa-save"/>
+            <select id='mode' value={this.props.mode} onChange={this.props.setMode}>
+              <option value='text'>text</option>
+              <option value='python'>python</option>
+              <option value='javascript'>javascript</option>
+              <option value='c_cpp'>C/C++</option>
+            </select>
           </div>
         )
     }
