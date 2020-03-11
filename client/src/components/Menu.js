fc77568ddfa6d2ac8eb2d767a4ad526a2fcf9f90
Tony Zhao <41414030+tawnyzhao@users.noreply.github.com>
2020-03-11 14:41:08 -0400

    add rest of languages

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -34,8 +34,27 @@ export default class Menu extends React.Component {
         <select id='mode' value={this.props.mode} onChange={this.props.setMode}>
           <option value='text'>text</option>
           <option value='python'>python</option>
-          <option value='javascript'>javascript</option>
-          <option value='c_cpp'>C/C++</option>
+          <option value='typescript'>javascript</option>
+          <option value='c_cpp'>c/c++</option>
+          <option value='java'>java</option>
+          <option value='csharp'>c#</option>
+          <option value='golang'>go</option>
+          <option value='objectivec'>objective c</option>
+          <option value='php'>php</option>
+          <option value='ruby'>ruby</option>
+          <option value='rust'>rust</option>
+          <option value='sql'>sql</option>
+          <option value='kotlin'>kotlin</option>
+          <option value='clojure'>clojure</option>
+          <option value='d'>d</option>
+          <option value='cobol'>cobol</option>
+          <option value='haskell'>haskell</option>
+          <option value='json'>json</option>
+          <option value='lua'>lua</option>
+          <option value='julia'>julia</option>
+          <option value='markdown'>markdown</option>
+          <option value='scala'>scala</option>
+          <option value='swift'>swift</option>
         </select>
       </div>
     )
