bdbe14cff28a56d7ef01eaaec7ad04aa0a3e760e
Jin Yi Li <39113059+jinyic@users.noreply.github.com>
2020-03-11 21:49:50 -0400

    add modal to menu

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,19 +1,52 @@
-import React from 'react';
-import Icon from './Icon';
-import './icons.css';
-import '../fonts/all';
+import React from "react";
+import Icon from "./Icon";
+import Modal from "./Modal";
+import "./icons.css";
+import "../fonts/all";
 
 export default class Menu extends React.Component {
-    print () {
-        console.log("click")
+    constructor() {
+        super();
+        this.state = {
+            showModal: false
+        };
+
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this)
     }
 
-    render () {
-        return(
-          <div className='menu'>
-            <Icon id='copy' onClick={this.props.copyClipboard} className="fas fa-copy"/>
-            <Icon id='save' onClick={this.props.submit} className="fas fa-save"/>
-          </div>
-        )
+    openModal() {
+        this.setState({
+            showModal: true
+        });
+    }
+
+    closeModal() {
+        this.setState({
+            showModal: false
+        })
+    }
+
+    render() {
+        return (
+            <div className="menu">
+                <Modal showModal={this.state.showModal} closeModal={this.closeModal}></Modal>
+                <Icon
+                    id="copy"
+                    onClick={this.openModal}
+                    className="fas fa-circle"
+                />
+                <Icon
+                    id="copy"
+                    onClick={this.props.copyClipboard}
+                    className="fas fa-copy"
+                />
+                <Icon
+                    id="save"
+                    onClick={this.props.submit}
+                    className="fas fa-save"
+                />
+            </div>
+        );
     }
 }
