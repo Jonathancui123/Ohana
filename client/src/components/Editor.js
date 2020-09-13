4906be03b4e9dd24f0481bd63b7a5f7dc43ecca7
Jonathan Cui <jonathancui123@gmail.com>
2020-09-13 12:37:04 -0400

    integrate firebase

--- client/src/components/Editor.js
+++ client/src/components/Editor.js
@@ -7,7 +7,7 @@ import "../utils/modeImport";
 export default class Editor extends Component {
 
     componentDidMount(){
-        // Your web app's Firebase configuration
+        // Firebase configuration for real-time collaboration on firepad
         var firebaseConfig = {
             apiKey: "AIzaSyAceJM2eYvADgHBHtwCFl1EJjdQlGBzJFk",
             authDomain: "codepals-11647.firebaseapp.com",
@@ -19,16 +19,33 @@ export default class Editor extends Component {
         };
         // Initialize Firebase
         firebase.initializeApp(firebaseConfig);
+        //// Get Firebase Database reference.
+        var firepadRef = this.getRef();      
+
+        //// Create ACE
+        var editor = ace.edit("firepad-container");
+        editor.setTheme("ace/theme/textmate");
+        var session = editor.getSession();
+        session.setUseWrapMode(true);
+        session.setUseWorker(false);
+        session.setMode("ace/mode/javascript");
+
+        //// Create Firepad.
+        var firepad = Firepad.fromACE(firepadRef, editor, {
+            defaultText: '// JavaScript Editing with Firepad!\nfunction go() {\n  var message = "Hello, world.";\n  console.log(message);\n}'
+        });
     }
 
     render() {
         
         return (
-            <div style={{
+            <div 
+            id="firepad-container"
+            style={{
                 height: '80vh',
                 margin: '0 20px 0 20px'
             }}>
-                <AceEditor
+                {/* <AceEditor
                     focus //Autofocus
                     placeholder={this.props.placeholder}
                     value={this.props.value}
@@ -55,8 +72,23 @@ export default class Editor extends Component {
                     }}
                     style={{
                         "fontFamily": 'Fira Code'
-                    }} />
+                    }} /> */}
             </div>
         );
     }
+
+    getRef() {
+        var ref = firebase.database().ref();
+        var hash = window.location.hash.replace(/#/g, '');
+        if (hash) {
+          ref = ref.child(hash);
+        } else {
+          ref = ref.push(); // generate unique location.
+          window.location = window.location + '#' + ref.key; // add it as a hash to the URL.
+        }
+        if (typeof console !== 'undefined') {
+          console.log('Firebase data: ', ref.toString());
+        }
+        return ref;
+      }
 }
