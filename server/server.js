6fdbcba4c0fb6ed214889328861d92dbf6939ba6
oustan10 <oustan10@gmail.com>
2020-09-17 00:18:27 -0400

    connected mongodb

--- server/server.js
+++ server/server.js
@@ -1,100 +1,43 @@
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
+  require("dotenv").config();
 }
+
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const hash = require("./hash.js");
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 5000;
+const dbURI = (
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URI
+    : "mongodb://localhost:27017/ohana"
+)
+
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 app.listen(PORT, () => console.log(`Ohana server listening on port ${PORT}`));
 
-// const MongoClient = require("mongodb").MongoClient;
-
-// const uri = process.env.DATABASE_URI;
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// let db;
-
-// client.connect(err => {
-//     if (err !== undefined) {
-//         console.log(err);
-//     }
-//     // TODO change database name to "CodePals"
-//     files = client.db("todo").collection("files");
-// });
-
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-});
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Database connected"))
+  .catch(err => console.log(`Error connecting to database: ${err}`))
 
 app.get("/", (req, res) => {
-    res.status(200);
-    res.send("Ohana means family")
+  res.status(200);
+  res.send("Ohana means family")
 });
 
 app.get("/newHash", (req, res) => {
-    let newHash = hash.hash58();
-    res.send(newHash);
+  let newHash = hash.hash58();
+  res.send(newHash);
 });
-
-
-// app.get("/raw/:id", (req, res) => {
-//     console.log(req.params.id);
-//     files
-//         .find({ _id: req.params.id })
-//         .limit(1)
-//         .forEach((document, err) => {
-//             if (err) {
-//                 console.log(err);
-//             }
-//             // TODO make sure document isnt empty
-//             if (document) {
-//                 res.send(document.data);
-//             } else {
-//                 res.sendStatus(301);
-//             }
-//         })
-//         .catch(err => {
-//             console.log(err);
-//         });
-// });
-
-// app.get("/:id", (req, res) => {
-//     files
-//         .find({ _id: req.params.id })
-//         .limit(1)
-//         .forEach((document, err) => {
-//             if (err) {
-//                 console.log(err);
-//             }
-//             // console.log(document)
-//             // TODO make sure document isnt empty
-//             if (document) {
-//                 res.json({
-//                     data: document.data
-//                 });
-//             } else {
-//                 res.sendStatus(301);
-//             }
-//         })
-//         .catch(err => {
-//             console.log(err);
-//         });
-// });
-
-// app.post("/upload", (req, res) => {
-//     let id = hash.hash58();
-//     let file = {
-//         _id: id,
-//         data: req.body.data
-//     };
-//     files.insertOne(file, (err, result) => {
-//         res.send({
-//             id: id
-//         });
-//         if (err) return console.log(err);
-//     });
-// });
