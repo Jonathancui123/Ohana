a93505236e9c813a4a141d89cb8f4a47101d5f40
oustan10 <oustan10@gmail.com>
2020-09-17 18:15:48 -0400

    remove extra app listen

--- server/server.js
+++ server/server.js
@@ -40,7 +40,6 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
-app.listen(PORT, () => console.log(`Ohana server listening on port ${PORT}`));
 
 mongoose
   .connect(dbURI, {
